8253f98fb21caf4090bf03fb0b147b337534f295
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2022-10-23 10:57:33 +0200

    start

---
+++ 00-terminal.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if Script is Run as Root
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user to run this script, please run sudo ./script-name.sh" 2>&1
+  exit 1
+fi
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+cd $builddir
+mkdir -p /home/$username/.config
+mkdir -p /home/$username/.fonts
+cp -R config/gtk-* /home/$username/.config/
+chown -R $username:$username /home/$username
+
+dconf reset -f /org/gnome/terminal/
+dconf load /org/gnome/terminal/ < terminal_settings.txt
+
+#apt update
+#apt install nala -y
