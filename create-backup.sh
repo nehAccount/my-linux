315a9853e92dcd2a4675cd9a0f48532cef6f907b
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-01 09:41:03 +0200

    backup corrections

--- create-backup.sh
+++ create-backup.sh
@@ -4,21 +4,28 @@ username=$(id -u -n 1000)
 builddir=$(pwd)
 
 cd $builddir
+
 # fonts
 echo ".fonts"
 cp -R /home/$username/.fonts/ $builddir/my-home/
+
 # icons
 echo ".icons"
 mkdir -p $builddir/my-home/.icons
 cp -R /home/$username/.icons/app-icons/ $builddir/my-home/.icons/
+
 # local
 echo ".local"
-mkdir -p $builddir/my-home/.local/share
-cp -R /home/$username/.local/share/applications/ $builddir/my-home/.local/share/
+mkdir -p $builddir/my-home/.local/share/applications
+cp /home/$username/.local/share/applications/geary-autostart.desktop $builddir/my-home/.local/share/applications/
+cp /home/$username/.local/share/applications/org.gnome.Geary.desktop $builddir/my-home/.local/share/applications/
+cp /home/$username/.local/share/applications/viber.appImage.desktop $builddir/my-home/.local/share/applications/
+
 # themes
 echo ".themes"
 mkdir -p $builddir/my-home/.themes
 cp -R /home/$username/.themes/MyTheme-* $builddir/my-home/.themes/
+
 # Templates
 echo "Templates"
 mkdir -p $builddir/my-home/Templates
