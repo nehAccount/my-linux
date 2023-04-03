84730436bfc5cefd098782085545aa3a8b4b324b
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-03 13:02:35 +0200

    flatpak

--- 20-snap.sh
+++ 20-snap.sh
@@ -6,9 +6,9 @@ builddir=$(pwd)
 cd "$builddir" ||exit
 
 sudo snap install libreoffice
-sudo snap install flameshot
+# sudo snap install flameshot
 sudo snap install photogimp
-sudo snap install postman
+# sudo snap install postman
 sudo snap install phpstorm --classic
 sudo snap install intellij-idea-community --classic
 sudo snap install code --classic
---
+++ 31-flatpak.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+flatpak install flathub com.viber.Viber -y
+flatpak install flathub fr.handbrake.ghb -y
+flatpak install flathub no.mifi.losslesscut -y
+flatpak install flathub org.flameshot.Flameshot -y
+flatpak install flathub com.getpostman.Postman -y
