25e9e535568a95703d325750f880ec24afc2fd88
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-03 13:05:55 +0200

    github

--- 31-flatpak.sh
+++ 31-flatpak.sh
@@ -8,3 +8,4 @@ flatpak install flathub fr.handbrake.ghb -y
 flatpak install flathub no.mifi.losslesscut -y
 flatpak install flathub org.flameshot.Flameshot -y
 flatpak install flathub com.getpostman.Postman -y
+flatpak install flathub io.github.shiftey.Desktop
