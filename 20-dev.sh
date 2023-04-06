ace44f9d9c250c2cd1875b97ea8048869d50c8c9
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-07 00:06:09 +0200

    mariadb test

--- 20-dev.sh
+++ 20-dev.sh
@@ -7,9 +7,24 @@ builddir=$(pwd)
 sudo snap install flutter --classic
 flutter doctor
 
+sudo nala update
+
+### MariaDB
+sudo nala install -y mariadb-server
+echo "### IMPORTANT "
+echo "Enter current password for root (enter for none): PRESS ENTER HERE"
+echo "Switch to unix_socket authentication [Y/n] n"
+echo "Set root password? [Y/n] n"
+echo "Disable remote access for root? [Y/n] Y"
+sudo mysql_secure_installation
+
+sudo mariadb -e "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+sudo mariadb -e "FLUSH PRIVILEGES;"
+sudo systemctl restart mariadb
+sudo mariadb -e "SELECT user FROM mysql.user;"
+
 ### Docker
 echo "Installing Docker..."
-sudo nala update
 sudo nala install -y ca-certificates gnupg
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
