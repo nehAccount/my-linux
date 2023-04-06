0d891352aaa24a33519b8f3e96e917b902a3176c
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-06 23:38:54 +0200

    mariadb test

--- test.sh
+++ test.sh
@@ -2,8 +2,19 @@
 
 username=$(id -u -n 1000)
 builddir=$(pwd)
-sudo mysql -u root -e "SELECT user FROM mysql. user;"
-sudo mysql -u root -e "show databases;"
+sudo nala update
+sudo nala install -y mariadb-server
+echo "### IMPORTANT "
+echo "Enter current password for root (enter for none): PRESS ENTER HERE"
+echo "Switch to unix_socket authentication [Y/n] n"
+echo "Set root password? [Y/n] n"
+sudo mysql_secure_installation
+
+#sudo mariadb -e "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;"
+#sudo mariadb -e "FLUSH PRIVILEGES;"
+#sudo systemctl restart mariadb
+#sudo mariadb -e "SELECT user FROM mysql.user;"
+
 
 
 
