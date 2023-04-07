2e6ba1ef8746767b1137cb64127bc1289a19519c
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-07 12:14:39 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,10 +3,16 @@
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
-sudo apt install php
+sudo nala install php
+sudo nala install php-json php-ctype php-curl php-mbstring php-xml php-zip php-tokenizer php-tokenizer libpcre3 --no-install-recommends
+sudo nala install php-mysql
+sudo nala install php-intl
+
 
 curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
 sudo apt install symfony-cli
+
+symfony check:requirements 
 
 
 
