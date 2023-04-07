499750c2ff0b9ecae27efbf42aef1bda6de2cd4e
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-07 11:45:01 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@
 username=$(id -u -n 1000)
 builddir=$(pwd)
 
+sudo apt install php
 
+curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
+sudo apt install symfony-cli
 
 
