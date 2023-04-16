3b4d35125a3531c0576a4e60801cb9825a991704
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-16 09:58:18 +0200

    bnp-zenica.com

--- 30-bnp.sh
+++ 30-bnp.sh
@@ -25,23 +25,24 @@ echo "Setting permissions for www-data"
 sudo chown -R www-data:www-data /var/www
 
 # symlink to /home/bnp-admin
-mkdir -p /home/"$username"/PhpProjects
-sudo ln -s /var/www/bnp-zenica.com /home/"$username"/PhpProjects/bnp-zenica.com
+sudo mkdir -p /home/"$username"/PhpProjects
+sudo chown -R $username:$username /home/$username/PhpProjects
+sudo ln -s /var/www/bnp-zenica.com /home/"$username"/PhpProjects
 
 # PHP 7.4
-sudo nala install software-properties-common -y
-sudo add-apt-repository ppa:ondrej/php -y
-sudo nala update
-sudo nala install -y php7.4 php7.4-fpm libapache2-mod-php7.4 php7.4-curl php7.4-intl php7.4-zip php7.4-soap php7.4-xml php7.4-gd php7.4-mbstring php7.4-bcmath php7.4-common php7.4-xml php7.4-mysqli
+#sudo nala install software-properties-common -y
+#sudo add-apt-repository ppa:ondrej/php -y
+#sudo nala update
+#sudo nala install -y php7.4 php7.4-fpm libapache2-mod-php7.4 php7.4-curl php7.4-intl php7.4-zip php7.4-soap php7.4-xml php7.4-gd php7.4-mbstring php7.4-bcmath php7.4-common php7.4-xml php7.4-mysqli
 
 # enable mods on apache
 sudo a2enmod ssl
 sudo a2enmod rewrite
-sudo a2enmod proxy_fcgi setenvif
-sudo a2enconf php7.4-fpm
+#sudo a2enmod proxy_fcgi setenvif
+#sudo a2enconf php7.4-fpm
 
 # setup php version for symfony
-echo 7.4 > /var/www/bnp-zenica.com/.php-version
+#echo 7.4 > /var/www/bnp-zenica.com/.php-version
 
 # setup site
 sudo cp "$builddir"/bnp/bnp-zenica.com.conf /etc/apache2/sites-available/
