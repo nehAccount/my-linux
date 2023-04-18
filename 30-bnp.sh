76b6fe107c5e610b2d9abf6837951c1bffa4ab5a
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-18 09:48:55 +0200

    bnp mariadb

--- 30-bnp.sh
+++ 30-bnp.sh
@@ -20,16 +20,16 @@ sudo chmod -R 777 "$rolesDir"
 
 # copy .env file
 cp "$builddir"/bnp/.env "$dirName"/"$siteName"
-cp "$builddir"/bnp/.env "$dirName"/"$siteName"/.env.dev
+cp "$builddir"/bnp/.env "$dirName"/"$siteName"/.env.local
 
 # set mariadb user
-#sudo mariadb -e "GRANT ALL ON *.* TO 'bnp-admin'@'localhost' IDENTIFIED BY 'Ngzmhr072' WITH GRANT OPTION;"
-#sudo mariadb -e "FLUSH PRIVILEGES;"
+sudo mariadb -e "GRANT ALL ON *.* TO 'bnp-admin'@'localhost' IDENTIFIED BY 'Ngzmhr072' WITH GRANT OPTION;"
+sudo mariadb -e "FLUSH PRIVILEGES;"
 
-#sudo systemctl restart mariadb
+sudo systemctl restart mariadb
 
 ### Confirmation
-#sudo mariadb -e "SELECT user FROM mysql.user;"
+sudo mariadb -e "SELECT user FROM mysql.user;"
 
 
 
