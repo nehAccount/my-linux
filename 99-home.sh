7b740965ee67b269d5aadc6f076bf606825454a2
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-04-25 01:32:06 +0200

    corrections

--- 99-home.sh
+++ 99-home.sh
@@ -16,10 +16,20 @@ chown -R $username:$username /home/$username
 sudo cp $builddir/scripts/update-all /usr/local/bin/
 sudo chmod +x /usr/local/bin/update-all
 
-### Dash to dock transparency (default: 0.8)
+### Dash to dock
+# transparency (default: 0.8)
 gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode 'FIXED'
 gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.99
+# mounts (hide gmail accounts)
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts 'false'
 
+### Desktop and windows
+# home icon on desktop
+gsettings set org.gnome.shell.extensions.ding show-home 'false'
+# hide maximize button on window title bar
+gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,close'
+# center windows
+gsettings set org.gnome.mutter center-new-windows 'true'
 
 
 
--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ builddir=$(pwd)
 # sudo nala update
 # sudo nala install -y hugo
 
+gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts 'true'
 
 
 
