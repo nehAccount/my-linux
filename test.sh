23eba21a273ba7e3393529edd41602b13e014e26
BNP Admin <bnp.zenica.web@gmail.com>
2023-05-18 15:08:32 +0200

    kde-test

--- test.sh
+++ test.sh
@@ -6,6 +6,12 @@ builddir=$(pwd)
 # sudo nala install -y hugo
 
 
+# debian
+# minimal installation
+sudo apt install kde-standard -y
+sudo apt install nala -y
+sudo nala install fonts-roboto -y
+
 
 
 
