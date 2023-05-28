aea6cd891926e61d2769833c64563aceb16e215e
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2023-05-28 17:08:31 +0200

    flutter

---
+++ debian-kde/25_snap.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# username=$(id -u -n 1000)
+
+#sudo nala update
+
+### SNAP and snap.service
+#sudo nala install snapd -y
+#sudo systemctl start snapd
+#sudo systemctl enable snapd
+
+### snap core
+#sudo snap install core
+
+### Flutter
+#sudo snap install flutter --classic
+#/snap/bin/flutter
+
+### Java SDK
+#sudo nala install -y openjdk-11-jdk
+#/snap/bin/flutter doctor --android-licenses
+
+
+#echo "# Add snap applications to PATH - added by Nermin" >> ~/.zshrc
+#echo "export PATH=\"\$PATH:/snap/bin\"" >> ~/.zshrc
+
+
+
+
+
