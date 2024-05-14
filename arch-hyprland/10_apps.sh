df4bbb29258893fa120afc129055aa0fa9c7f6bc
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2024-05-14 21:27:07 +0200

    apps

--- arch-hyprland/10_apps.sh
+++ arch-hyprland/10_apps.sh
@@ -5,16 +5,32 @@ builddir=$(pwd)
 fonts_dir="/usr/share/fonts"
 
 # update system
-sudo pacman -Sy
 sudo pacman -Syu
-sudo pacman -Syyu
 
 sudo pacman -S --needed nautilus gnome-text-editor
+sudo pacman -S --needed nextcloud-client
+sudo pacman -S --needed chromium
+sudo pacman -S --needed thunderbird systray-x-kde
+sudo pacman -S --needed libreoffice-fresh
+sudo pacman -S --needed gimp
+sudo pacman -S --needed mpv
+sudo pacman -S --needed flameshot
+sudo pacman -S --needed timeshift
+sudo pacman -S --needed ufw gufw
+sudo pacman -S --needed partitionmanager
+sudo pacman -S --needed pandoc-cli
+sudo pacman -S --needed img2pdf
+#sudo pacman -S --needed handbrake
+#sudo pacman -S --needed intellij-idea-community-edition
+
+flatpak install flathub com.viber.Viber -y
+flatpak install flathub no.mifi.losslesscut -y
+
+yay -S --needed phpstorm phpstorm-jre
 
 echo "DONE!"
 
-########################
-## zsh
+
 
 
 
