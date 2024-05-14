f77ec2d04f7e592da6433d5a969bdc6621474fe5
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2024-05-14 11:52:33 +0200

    settings backup

---
+++ arch-hyprland/settings/backup-current.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+clear
+
+username=$(id -u -n 1000)
+builddir=$(pwd)
+
+mkdir -p "$builddir/dotfiles"
+mkdir -p "$builddir/.config"
+
+# Stephan Raabe dotfiles
+dotfiles_dir="/home/$username/dotfiles"
+
+# My files
+dotfiles_nermin_dir="$builddir/dotfiles"
+hypr_conf_dir="$dotfiles_nermin_dir/hypr/conf"
+
+echo "This will backup my changes in dotfiles."
+echo ""
+while true; do
+    read -p "Do you really want to backup settings? (Yy/Nn): " yn
+    case $yn in
+        [Yy]* )
+		echo "Custom conf"	    	
+		mkdir -p $hypr_conf_dir
+		cp $dotfiles_dir/hypr/conf/custom.conf $hypr_conf_dir/
+
+		echo "Alacritty"
+		mkdir -p $dotfiles_nermin_dir/alacritty
+		cp $dotfiles_dir/alacritty/alacritty.toml $dotfiles_nermin_dir/alacritty/
+
+		echo "Hyprland changes: borders, blur..."		
+		mkdir -p "$builddir/.config/ml4w-hyprland-settings"
+		cp /home/$username/.config/ml4w-hyprland-settings/hyprctl.json $builddir/.config/ml4w-hyprland-settings/
+        break;;
+        [Nn]* ) 
+	    echo "Nothig is copied."
+            exit
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
