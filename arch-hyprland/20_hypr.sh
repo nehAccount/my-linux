095cb407f19cee633a725926dc4def4be6306b22
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2024-05-29 18:04:09 +0200

    kde

--- arch-hyprland/20_hypr.sh
+++ arch-hyprland/20_hypr.sh
@@ -5,6 +5,11 @@ sudo pacman -S --needed swaync
 # KDE Apps and Settings
 sudo pacman -S --needed dolphin kate qt5ct qt6ct
 
+# File association in dolphin.
+# delete plasma- prefix after copy
+# cp /etc/xdg/menus/plasma-applications.menu ~/.config/menus/
+# run once: kbuildsycoca6
+
 # fonts
 yay nerd-fonts-noto-sans-regular-complete
 fc-cache -f -v
