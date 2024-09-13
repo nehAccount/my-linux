5106d7ffe06a4a45b9b278c579f88042212d336c
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2024-09-13 13:03:24 +0200

    gtk style

--- arch-hyprland/20_hypr.sh
+++ arch-hyprland/20_hypr.sh
@@ -5,6 +5,11 @@ sudo pacman -S --needed swaync
 # KDE Apps and Settings
 sudo pacman -S --needed dolphin kate qt5ct qt6ct
 
+# GTK Themes
+sudo pacman -S --needed materia-gtk-theme
+# GTK Icons
+yay -S suru-plus-telinkrin-git
+
 # File association in dolphin.
 # delete plasma- prefix after copy
 # cp /etc/xdg/menus/plasma-applications.menu ~/.config/menus/
