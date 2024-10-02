1c4c46d895ce01b7c914a0650d7e325f480eddf5
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2024-10-02 21:41:49 +0200

    mission center

--- arch-hyprland/10_apps.sh
+++ arch-hyprland/10_apps.sh
@@ -27,6 +27,7 @@ sudo pacman -S --needed intellij-idea-community-edition
 flatpak install flathub com.viber.Viber -y
 flatpak install flathub no.mifi.losslesscut -y
 flatpak install flathub com.jgraph.drawio.desktop -y
+flatpak install flathub io.missioncenter.MissionCenter -y
 
 # PhpStorm
 yay -S --needed phpstorm phpstorm-jre
