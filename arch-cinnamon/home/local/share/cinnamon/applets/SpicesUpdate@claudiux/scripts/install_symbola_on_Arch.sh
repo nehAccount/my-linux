fd89ef6aac925c20416865c48fc0062431c59452
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2025-03-13 21:27:12 +0100

    shares

---
+++ arch-cinnamon/home/local/share/cinnamon/applets/SpicesUpdate@claudiux/scripts/install_symbola_on_Arch.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+USRFONTSDIR="$HOME/.local/share/fonts"
+APPLETFONTSDIR="$HOME/.local/share/cinnamon/applets/SpicesUpdate@claudiux/fonts/Symbola"
+URLDOWNLOAD="https://raw.githubusercontent.com/claudiux/fonts/master/Symbola/Symbola.tar.gz"
+
+INSTALLED=$(fc-list Symbola --format="%{family[0]}\n" | sort | uniq)
+
+if [ -z $INSTALLED ]
+then
+  mkdir -p "${USRFONTSDIR}"
+  cd "${APPLETFONTSDIR}"
+  curl -s -o Symbola.tar.gz ${URLDOWNLOAD} && {
+    tar xzf Symbola.tar.gz
+    cp "Symbola.otf" "${USRFONTSDIR}"
+    [[ -f /etc/gentoo-release ]] || {
+      fc-cache -f
+    }
+    rm -f Symbola.tar.gz
+  }
+fi
+
+if [ -f /etc/gentoo-release ]
+then
+  fc-cache -f
+fi
+
+exit 0
