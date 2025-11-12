9d614550f7510987a4c22f3820af62b8a01f0be2
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2025-11-12 12:08:40 +0100

    postgres

--- arch-cinnamon/postgres.sh
+++ arch-cinnamon/postgres.sh
@@ -1,39 +1,27 @@
 #!/bin/bash
 
-#username=$(id -u -n 1000)
-builddir=$(pwd)
+# username=$(id -u -n 1000)
+# builddir=$(pwd)
 
-# Skripta za instalaciju i inicijalizaciju PostgreSQL na Arch Linuxu
-
-# Ažuriranje sistema
-echo "Ažuriram sistem..."
 sudo pacman -Syu
 
-# Instalacija PostgreSQL paketa
 echo "Instaliram PostgreSQL..."
 sudo pacman -S postgresql
 
-# Inicijalizacija baze podataka
 echo "Inicijalizujem bazu podataka..."
 sudo -u postgres initdb --locale en_US.UTF-8 -D /var/lib/postgres/data
 
-# Pokretanje PostgreSQL servisa
-echo "Pokrećem PostgreSQL servis..."
+echo "Pokrećem servis..."
 sudo systemctl start postgresql
-
-# Omogućavanje automatskog pokretanja PostgreSQL-a pri startupu
-echo "Omogućavam PostgreSQL da se pokreće pri startupu..."
 sudo systemctl enable postgresql
 
-# Kreiranje PostgreSQL korisnika (ako je potrebno, možeš promeniti ime korisnika)
-echo "Kreiram PostgreSQL korisnika 'postgres'..."
+echo "Kreiram korisnika 'postgres'..."
 sudo -i -u postgres psql -c "CREATE USER postgres WITH SUPERUSER PASSWORD 'postgres';"
 
-# Status servisa
-echo "Status PostgreSQL servisa:"
+echo "Status servisa:"
 sudo systemctl status postgresql
 
-# Poruka o završetku
-echo "PostgreSQL je uspešno instaliran i pokrenut!"
-
+echo "Instaliram pgAdmin:"
 flatpak install flathub org.pgadmin.pgadmin4
+
+echo "PostgreSQL je uspešno instaliran i pokrenut!"
