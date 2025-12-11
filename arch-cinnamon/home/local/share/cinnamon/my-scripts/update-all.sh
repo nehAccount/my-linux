f9fc0cff0be0141973b58c52e46a65bc22ab3fed
Nermin Hadzimejlic <nermin.hadzimejlic@gmail.com>
2025-12-11 10:14:12 +0100

    update-all.sh

--- arch-cinnamon/home/local/share/cinnamon/my-scripts/update-all.sh
+++ arch-cinnamon/home/local/share/cinnamon/my-scripts/update-all.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo ":: Full system and applications update"
-echo " "
+echo ":::::::::::::::::::::::::::::::::::::::::::"
 
 # update packages
 yay
 # update flatpak
 echo "Starting flatpak update..."
-sleep 3
+# sleep 3
 flatpak update
 flatpak remove --unused
-sleep 3
+# sleep 3
 
 # exit messages
 send-notification "Update completed" "All applications successfully updated"
